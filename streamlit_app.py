0448520464b8535477809bf0f11e694af3002c49
mkhisamnew <168507530+mkhisamnew@users.noreply.github.com>
2024-04-30 01:06:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title("Customer Your Smoothie :cup_with_straw:")
-st.write("Choose the fruits you want in your custom Smoothie")
+st.write("Choose the fruits you want in your custom Smoothie" )
 
 
 
